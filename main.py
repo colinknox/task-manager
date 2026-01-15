0ef428a6a95bbfa075eb7da93da1d593f68f2705
colinknox <colinwknox@gmail.com>
2026-01-14 19:33:13 -0500

    added add_task and remove_task methods

--- main.py
+++ main.py
@@ -7,4 +7,26 @@ class Task:
 class TaskList:
     def __init__(self, list_name):
         self.list_name = list_name
-        tasks_list = []
+        self.tasks_list = []
+
+    def add_task(self, task):
+        self.tasks_list.append(task)
+
+    def remove_task(self, task):
+        updated_list = []
+        for current_task in self.tasks_list:
+            if current_task.description.lower() != task.description.lower():
+                updated_list.append(current_task)
+
+        return updated_list
+
+
+task_one = Task("Get a gallon of milk", "High", "Pending")
+task_two = Task("Chew gum", "Medium", "Completed")
+grocery_list = TaskList("Grocery List")
+
+# print(f"BEFORE CURRENT ITEMS IN TASKS LIST: {grocery_list.tasks_list}")
+grocery_list.add_task(task_one)
+grocery_list.add_task(task_two)
+print(f"BEFORE CURRENT ITEMS IN TASKS LIST: {grocery_list.tasks_list}")
+print(grocery_list.remove_task(task_one))
